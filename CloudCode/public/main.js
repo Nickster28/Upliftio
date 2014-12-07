4947d5b54dffa585179151cc063cc91c60e5a6e1
Nick Troccoli <nicktroccoli28@gmail.com>
2014-12-07 02:56:28 -0800

    Fixed error message display in web client, commented code.  Also reported success in client as well

--- CloudCode/public/main.js
+++ CloudCode/public/main.js
@@ -2,27 +2,27 @@
 Parse.initialize("875InjBieGp3C0KQImOX9Pt884YTn8ihl7NuNkhj", "hDYLGDxVOwnGSaeQg9FZLpyVAGCKGxzl944A9Yoy");
 
 $(document).ready(function(){
-	
 	initializePage();
 });
 
+// Set up the click handler for the submit button
 var initializePage = function(){
 	$("button").click(function(e){
 		e.preventDefault();
 		var message = $("#message").val();
 		var password = $("#password").val();
+
+		// Clear out the text fields
 		$("#message").val("");
 		$("#password").val("");
-		var from = $(this).text();
-		console.log(from);
-		console.log(message);
+
+		// Call the cloud function to send the message to all users
 		Parse.Cloud.run('sendInspiration', {message: message, password: password}, {
 			success: function(data){
-
+				$("button").text("Mesage sent!  Check Cloud Code logs for individual successes/errors");
 			},
 			error:function(err){
-				$("button").text("Error: "+ err);
-				console.log(err);
+				$("button").text("Error: " + err.message);
 			}
 		});
 
